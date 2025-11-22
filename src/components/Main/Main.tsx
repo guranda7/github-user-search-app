657f6fb571292740dd9a20e9017052ca4f8e234a
guranda <gurandaggg@gmail.com>
2025-11-22 20:06:30 +0400

    start the project

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import Search from "../Search/Search"
+
+export default function Main() {
+    
+   
+  return (
+    <div>
+        <Search/>
+    </div>
+  )
+}
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,47 @@
+
+import { useState, useEffect } from 'react'
+
+interface UserType  {
+    name: string; 
+    login: string; 
+    bio: string | null;
+    folowers: number;
+    following: number;
+    location: string | null;
+    created_at: number;
+    avatar_url: string;
+}
+
+export default function Search() {
+    const [user, setUser] = useState<UserType | undefined>()
+    const [search, setSearch] = useState<string>("")
+     useEffect(() => {
+        getUser()
+    }, [])
+
+    const getUser = async () => {
+        try{
+            const response = await fetch(`https://api.github.com/users/${search}`);
+          const data = await response.json()
+        console.log(data)
+        console.log(user)
+         setUser(data)
+        } catch(error) {
+            console.log(error)
+        }
+
+       
+    }
+  return (
+    <div>
+        <input type="text" value={search} onChange={(event) => {
+            setSearch(event.target.value)
+        }} />
+        <button onClick={getUser}>Search</button>
+
+        <h1>user name : {user?.name}</h1>
+        <h2>Login : {user?.login}</h2>
+        <p>location: {user?.location}</p>
+    </div>
+  )
+}
